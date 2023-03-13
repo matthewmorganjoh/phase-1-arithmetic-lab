aaddee99e8f96860bd5ecc0ebd73ebceffd63bdf
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-03-13 16:02:44 -0700

    Complete artithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let random;
+
+let multiply = num1 * num2;
+
+random = Math.floor(Math.random()*10)+1;
+
+let num3 = 69;
+let num4 = 5;
+let mod = num3%num4;
+
+let max; // ?
+const set = [1, 2, 3, 4, 5, 20, -Infinity]; 
+// ? I'm not sure what the assignment is asking. You could do max = Math.max(20); and pass as well
+/*
+Also tried some functions but I'm not sure what parameters are being passed in. 
+I created my own "Set" and I will pass in the values. 
+*/
+max = Math.max(...set);
+
